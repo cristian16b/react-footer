2fc0ffbf5abcae6aaaa17780a5c0b23735d2ee53
cristian16b <cristian.budzicz@gmail.com>
2020-12-04 19:46:38 -0300

    Se modifica la estructura del footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,103 +4,100 @@ const Footer = () => {
   return (
     // <!-- Footer -->
     <footer className="fondo">
-    
       {/* <!-- Footer Links --> */}
       <div className="container text-center text-md-left">
-    
         {/* <!-- Grid row --> */}
         <div className="row">
-    
           {/* <!-- Grid column --> */}
-          <div className="col-md-3 mx-auto">
-    
-            <div className="row">
-              <div className="col-md-3 mx-auto">
-                {/* <!-- Links --> */}
-                <p className="titulo">Nuestros sitios</p>
-                  
-                  <ul className="list-unstyled">
-                    <li>
-                      <a className="enlace-footer" href="#!">Very long link 1</a>
-                    </li>
-                  </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-3 mx-auto">
-                {/* <!-- Links --> */}
-                <p className="titulo">Seguinos</p>
-                  
-                  <ul className="list-unstyled">
-                    <li>
-                      <a className="enlace-footer" href="#!">Very long link 1</a>
-                    </li>
-                  </ul>
-              </div>
-            </div>
-    
-          </div>
+          <>{primeraColumna()}</>
           {/* <!-- Grid column --> */}
-    
-          <hr className="clearfix w-100 d-md-none" />
-    
           {/* <!-- Grid column --> */}
-          <div className="col-md-3 mx-auto">
-    
-            {/* <!-- Links --> */}
-            <p className="titulo">Secciones</p>
-    
-            <ul className="list-unstyled">
-              <li>
-                <a className="enlace-footer" href="#!">Link 1</a>
-              </li>
-            </ul>
-    
-          </div>
+          <>{segundaColumna()}</>
           {/* <!-- Grid column --> */}
-    
-          <hr className="clearfix w-100 d-md-none" />
-    
           {/* <!-- Grid column --> */}
-          <div className="col-md-3 mx-auto">
-    
-            {/* <!-- Links --> */}
-            <p className="titulo">Figuras</p>
-    
-            <ul className="list-unstyled">
-              <li>
-                <a className="enlace-footer" href="#!">Link 1</a>
-              </li>
-            </ul>
-    
-          </div>
+          <>{terceraColumna()}</>
           {/* <!-- Grid column --> */}
-    
-          <hr className="clearfix w-100 d-md-none" />
-    
           {/* <!-- Grid column --> */}
-          <div className="col-md-3 mx-auto">
-    
-            {/* <!-- Links --> */}
-            <p className="titulo">Institucional</p>
-    
-            <ul className="list-unstyled">
-              <li>
-                <a className="enlace-footer" href="#!">Link 1</a>
-              </li>
-            </ul>
-    
-          </div>
+          <>{cuartaColumna()}</>
           {/* <!-- Grid column --> */}
-    
         </div>
         {/* <!-- Grid row --> */}
-    
       </div>
       {/* <!-- Footer Links --> */}
-    
     </footer>
   );
+}
+
+const primeraColumna = () => {
+  return(
+    <div className="col-md-3 mx-auto">
+      <div className="row">
+        <div className="col-md-3 mx-auto">
+          {/* <!-- Links --> */}
+          <p className="titulo">Nuestros sitios</p>
+            
+            <ul className="list-unstyled">
+              <li>
+                <a className="enlace-footer" href="#!">Very long link 1</a>
+              </li>
+            </ul>
+        </div>
+    </div>
+    <div className="row">
+      <div className="col-md-3 mx-auto">
+        {/* <!-- Links --> */}
+        <p className="titulo">Seguinos</p>
+          <ul className="list-unstyled">
+            <li>
+              <a className="enlace-footer" href="#!">Very long link 1</a>
+            </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+const segundaColumna = ()  => {
+  return(
+    <div className="col-md-3 mx-auto">
+    {/* <!-- Links --> */}
+      <p className="titulo">Secciones</p>
+      <ul className="list-unstyled">
+      <li>
+        <a className="enlace-footer" href="#!">Link 1</a>
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+const terceraColumna = () => {
+  return (
+    <div className="col-md-3 mx-auto">
+      {/* <!-- Links --> */}
+      <p className="titulo">Figuras</p>
+      <ul className="list-unstyled">
+        <li>
+          <a className="enlace-footer" href="#!">Link 1</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const cuartaColumna = () => {
+  return(
+    <div className="col-md-3 mx-auto">
+    {/* <!-- Links --> */}
+      <p className="titulo">Institucional</p>
+      <ul className="list-unstyled">
+        <li>
+          <a className="enlace-footer" href="#!">Link 1</a>
+        </li>
+      </ul>
+  </div>
+  )
 }
 
 export default Footer;
