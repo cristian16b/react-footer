59fd0190eb50c4132df407d3f2abea02d7eae1dc
cristian16b <cristian.budzicz@gmail.com>
2020-12-04 13:28:48 -0300

    Agrego el cdn de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer/Footer';
+import PreFooter from './PreFooter/PreFooter';
 
 function App() {
   return (
     <div className="App">
+      <PreFooter/>
+      <Footer/>
     </div>
   );
 }
